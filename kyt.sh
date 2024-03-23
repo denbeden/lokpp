365b2d181bebbd226831f56b674335c54f8b15f6
ALAWI-VPN <alawivpn@gmail.com>
2024-03-23 18:21:03 +0700

    Update add file

--- kyt.sh
+++ kyt.sh
@@ -18,6 +18,7 @@ unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
 #isi data
+clear
 echo ""
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
@@ -26,8 +27,8 @@ echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
 echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
 echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -e -p "[*] Input your Bot Token : " bottoken
-read -e -p "[*] Input Your Id Telegram :" admin
+read -e -p "[*] Input your Bot Token: " bottoken
+read -e -p "[*] Input Your Id Telegram: " admin
 echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
 echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
 echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
@@ -47,7 +48,8 @@ Restart=always
 WantedBy=multi-user.target
 END
 
-systemctl start kyt 
+systemctl daemon-reload
+systemctl start kyt
 systemctl enable kyt
 systemctl restart kyt
 cd /root
